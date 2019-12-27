f6d74901c1e75d1e3b630a8eb2cdc625dc8bed81
Stefano Salsano <stefano.salsano@uniroma2.it>
2019-12-28 00:52:50 +0100

    initial commit

---
+++ nets/8routers/nodeconf/h2/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BASE_DIR=/home/user/mytests/ospf3routers/nodeconf
+NODE_NAME=h2
+GW_NAME=r2
+IF_NAME=$NODE_NAME-$GW_NAME
+IP_ADDR=10.102.0.2/16
+GW_ADDR=10.102.0.1
+
+ip addr add $IP_ADDR dev $IF_NAME 
+ip route add default via $GW_ADDR dev $IF_NAME
